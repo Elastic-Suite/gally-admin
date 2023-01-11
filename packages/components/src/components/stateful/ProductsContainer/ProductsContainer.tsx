9ba2b6ecb204d03e35e672b4d5a4ad7b0c608d81
Tony CABAYE <tony.cabaye@smile.fr>
2023-01-11 11:46:21 +0100

    fix: wrong import of react-i18next

--- packages/components/src/components/stateful/ProductsContainer/ProductsContainer.tsx
+++ packages/components/src/components/stateful/ProductsContainer/ProductsContainer.tsx
@@ -6,7 +6,7 @@ import React, {
   useState,
 } from 'react'
 import { Box, styled } from '@mui/system'
-import { useTranslation } from 'react-i18next'
+import { useTranslation } from 'next-i18next'
 
 import {
   ICategory,
