a4d3e2ffd0abec036eb4a5f7fd37df30d6c4d977
Ponefar <adrienwolf1@gmail.com>
2023-01-26 15:49:07 +0100

    fix: add filters in useEffect

--- packages/components/src/pages/admin/merchandize/categories.tsx
+++ packages/components/src/pages/admin/merchandize/categories.tsx
@@ -97,7 +97,18 @@ function AdminMerchandizeCategories(): JSX.Element {
   const { update, create } =
     useResourceOperations<ICategoryConfiguration>(catConfResource)
   useEffect(() => {
-    if (selectedCategoryItem?.id) {
+    if (
+      selectedCategoryItem?.id &&
+      categories.status === LoadStatus.SUCCEEDED
+    ) {
+      return null
+    }
+
+    if (
+      categories?.data?.categories.find(
+        (item) => item.id === selectedCategoryItem?.id
+      )
+    ) {
       fetchApi<ICategoryConfiguration>(
         `${catConfResource.url}/category/${selectedCategoryItem.id}`,
         filters
@@ -110,7 +121,7 @@ function AdminMerchandizeCategories(): JSX.Element {
       })
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [fetchApi, selectedCategoryItem?.id, catConfResource.url])
+  }, [fetchApi, selectedCategoryItem?.id, catConfResource.url, categories])
 
   const isValid = !catConf?.isVirtual || isRuleValid(catConf?.virtualRule)
 
