47bd912e448fe602293c7ab083a67f5107056750
Ponefar <adrienwolf1@gmail.com>
2023-02-10 17:48:56 +0100

    fix: update ESPP_700-textFieldTags

---
+++ packages/components/src/components/atoms/form/TextFieldTags.stories.tsx
@@ -0,0 +1,144 @@
+import React, { FormEvent, useState } from 'react'
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+
+import TextFieldTagsComponent from './TextFieldTags'
+import { ITextFieldTags } from '@elastic-suite/gally-admin-shared'
+
+export default {
+  title: 'Atoms/Form/TextFieldTags',
+  helperIcon: {
+    options: ['', 'information-circle', 'checkmark', 'close'],
+    control: { type: 'select' },
+  },
+  helperText: {
+    control: 'text',
+  },
+  component: TextFieldTagsComponent,
+} as ComponentMeta<typeof TextFieldTagsComponent>
+
+const mocksData: ITextFieldTags[] = [
+  {
+    label: 'Search terms',
+    id: 'search_terms',
+    data: [
+      {
+        label: 'chipOne',
+        id: 1,
+      },
+      {
+        label: 'chipTwo',
+        id: 2,
+      },
+      {
+        label: 'chipThree',
+        id: 3,
+      },
+      {
+        label: 'chipFour',
+        id: 4,
+      },
+    ],
+  },
+]
+
+const Template: ComponentStory<typeof TextFieldTagsComponent> = (args) => {
+  const [data, setData] = useState(mocksData)
+  const [inputVal, setInputVal] = useState<Record<string, string>>()
+
+  function onRemoveDataTag(idItem: string, idTag: number): void {
+    const newData = data.map((item) => {
+      if (item.id === idItem) {
+        const newTagsList = item.data.filter((item) => item?.id !== idTag)
+        return { ...item, data: newTagsList }
+      }
+      return item
+    })
+
+    return setData(newData)
+  }
+
+  function generateId(
+    listItems: { label: string; id: number }[],
+    index?: number
+  ): number {
+    let count = index ?? 0
+
+    if (listItems.find((item) => item.id === count)) {
+      return generateId(listItems, ++count)
+    }
+    return count
+  }
+
+  function onAddDataTag(idItem: string): void | null {
+    if (inputVal?.[idItem] === undefined || inputVal?.[idItem].trim() === '') {
+      return null
+    }
+
+    const newData = data.map((item) => {
+      if (item.id === idItem) {
+        const createId = generateId(item.data)
+        const newTagsList = [
+          ...item.data,
+          { label: inputVal?.[idItem], id: createId },
+        ]
+
+        return { ...item, data: newTagsList }
+      }
+      return item
+    })
+
+    setData(newData)
+    return setInputVal({ ...inputVal, [idItem]: '' })
+  }
+
+  function onChange(
+    idItem: string,
+    idTag: number | undefined,
+    event?: FormEvent<HTMLFormElement>
+  ): void | null {
+    if (event) {
+      event.preventDefault()
+      return onAddDataTag(idItem)
+    }
+    return onRemoveDataTag(idItem, idTag as number)
+  }
+
+  function onChangeInput(idItem: string, value: string): void {
+    return setInputVal({ ...inputVal, [idItem]: value })
+  }
+
+  return (
+    <>
+      {data.map((item) => {
+        const value = inputVal?.[item.id]
+        return (
+          <TextFieldTagsComponent
+            {...args}
+            data={item}
+            onChange={onChange}
+            value={value}
+            onChangeInput={onChangeInput}
+            key={item.id}
+          />
+        )
+      })}
+    </>
+  )
+}
+export const Default = Template.bind({})
+Default.args = {
+  margin: 'normal',
+  required: true,
+  helperText: 'Good',
+  helperIcon: 'information-circle',
+  label: 'Label',
+  disabled: false,
+  error: false,
+  fullWidth: false,
+  infoTooltip: '',
+}
+
+export const Disabled = Template.bind({})
+Disabled.args = {
+  disabled: true,
+}
---
+++ packages/components/src/components/atoms/form/TextFieldTags.tsx
@@ -0,0 +1,168 @@
+import React, { FormEvent, ReactNode } from 'react'
+import { styled } from '@mui/system'
+import Chip from '../Chip/Chip'
+import InputText from './InputText'
+import { ITextFieldTags } from '@elastic-suite/gally-admin-shared'
+import { FormHelperText, InputLabel } from '@mui/material'
+import IonIcon from '../IonIcon/IonIcon'
+import InfoTooltip from './InfoTooltip'
+import FormControl from './FormControl'
+
+const CustomRootTextFieldTags = styled('div')(({ theme }) => ({
+  paddingTop: theme.spacing(1.5),
+  paddingBottom: theme.spacing(1.5),
+  paddingRight: theme.spacing(2),
+  paddingLeft: theme.spacing(2),
+  borderRadius: theme.spacing(1),
+  border: '1px solid',
+  borderColor: theme.palette.colors.neutral[600],
+  width: '350px',
+  boxSizing: 'border-box',
+  gap: theme.spacing(2),
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  minHeight: '54px',
+}))
+
+const CustomRootTextFieldTagsDisabled = styled(CustomRootTextFieldTags)(
+  ({ theme }) => ({
+    paddingRight: theme.spacing(2),
+    paddingLeft: theme.spacing(2),
+    borderColor: theme.palette.colors.neutral[300],
+    color: theme.palette.colors.neutral[500],
+    background: theme.palette.colors.neutral[300],
+    width: 'auto',
+    maxWidth: '350px',
+    minWidth: '180px',
+  })
+)
+
+const CustomTags = styled('div')(({ theme }) => ({
+  display: 'flex',
+  gap: theme.spacing(1),
+  flexDirection: 'row',
+  flexWrap: 'wrap',
+  alignItems: 'center',
+}))
+
+const CustomLabelDisabled = styled('div')({
+  fontSize: '14px',
+  fontWeight: '400',
+  cursor: 'default',
+})
+
+export interface ITextFIeldTagsForm {
+  disabled?: boolean
+  error?: boolean
+  fullWidth?: boolean
+  infoTooltip?: string
+  helperText?: ReactNode
+  helperIcon?: string
+  label?: string
+  margin?: 'none' | 'dense' | 'normal'
+  required?: boolean
+}
+
+export interface ITextFieldTag extends ITextFIeldTagsForm {
+  data: ITextFieldTags
+  onChange: (
+    idItem: string,
+    idTag: number | undefined,
+    event?: FormEvent<HTMLFormElement>
+  ) => void
+  value: string | undefined
+  onChangeInput: (idItem: string, value: string) => void
+}
+
+function TextFieldTags(props: ITextFieldTag): JSX.Element {
+  const {
+    data,
+    onChange,
+    value,
+    onChangeInput,
+    disabled,
+    required,
+    error,
+    fullWidth,
+    helperText,
+    helperIcon,
+    label,
+    margin,
+    infoTooltip,
+  } = props
+
+  const isDisabled = Boolean(data.data.find((a) => a?.id === -1)) || disabled
+  const CustomRoot = isDisabled
+    ? CustomRootTextFieldTagsDisabled
+    : CustomRootTextFieldTags
+
+  return (
+    <FormControl error={error} fullWidth={fullWidth} margin={margin}>
+      {Boolean(label || infoTooltip) && (
+        <div style={{ marginBottom: '24px' }}>
+          <InputLabel shrink htmlFor={data.id} required={required}>
+            {label}
+            {infoTooltip ? <InfoTooltip title={infoTooltip} /> : null}
+          </InputLabel>
+        </div>
+      )}
+      <CustomRoot>
+        <CustomTags>
+          {data.data.map((item) => {
+            return isDisabled ? (
+              item.id === -1 && (
+                <CustomLabelDisabled>{item.label}</CustomLabelDisabled>
+              )
+            ) : (
+              <Chip
+                key={item.id}
+                label={item.label}
+                onDelete={(): void => onChange(data.id, item.id)}
+              />
+            )
+          })}
+          {!isDisabled && (
+            <form
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                alignItems: 'center',
+              }}
+              onSubmit={(e): void => onChange(data.id, undefined, e)}
+            >
+              <InputText
+                value={value}
+                size="small"
+                sx={{
+                  minWidth: 'auto',
+                  width: '146px',
+                  border: 'none',
+                  background: 'inherit',
+                  '&.MuiInputBase-root': { minHeight: 0, paddingLeft: '8px' },
+                }}
+                placeholder={data.label}
+                onChange={(value): void =>
+                  onChangeInput(data.id, value as string)
+                }
+              />
+            </form>
+          )}
+        </CustomTags>
+      </CustomRoot>
+      {Boolean(helperText) && (
+        <FormHelperText error={error}>
+          {Boolean(helperIcon) && (
+            <IonIcon
+              name={helperIcon as string}
+              style={{ fontSize: 18, marginRight: 2 }}
+            />
+          )}
+          {helperText}
+        </FormHelperText>
+      )}
+    </FormControl>
+  )
+}
+
+export default TextFieldTags
