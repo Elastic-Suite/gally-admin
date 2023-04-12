1c664ec66f57814056a7ae2dddd087977ed495e8
Ponefar <adrienwolf1@gmail.com>
2023-04-12 12:53:38 +0200

    feat: #1191180 Manage display of multiple values on tables

--- packages/components/src/components/stateful/FieldGuesser/FieldGuesser.tsx
+++ packages/components/src/components/stateful/FieldGuesser/FieldGuesser.tsx
@@ -6,7 +6,6 @@ import ReadableFieldGuesser from './ReadableFieldGuesser'
 
 function FieldGuesser(props: IFieldGuesserProps): JSX.Element {
   const { editable, ...fieldProps } = props
-
   if (editable) {
     return <EditableFieldGuesser {...fieldProps} />
   }
