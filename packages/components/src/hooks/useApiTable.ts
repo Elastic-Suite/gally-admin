dddc15804692a672aae696ae14b69d3b26b42b59
Ponefar <adrienwolf1@gmail.com>
2023-06-05 19:50:22 +0200

    feat: refacto-form-Builder

--- packages/components/src/hooks/useApiTable.ts
+++ packages/components/src/hooks/useApiTable.ts
@@ -35,7 +35,7 @@ export function useApiHeadersForm(
       Record<string, IFieldConfigFormWithFieldset>
     >(
       (acc, header) => {
-        const fieldsetCode = header?.field?.gally?.fieldset
+        const fieldsetCode = header.fieldset
         const fieldset = resource.gally?.fieldset?.[fieldsetCode]
         if (fieldsetCode && fieldset) {
           if (fieldsetCode in acc) {
