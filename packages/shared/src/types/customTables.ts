e830ecccd416e44b1bd767797613eb56d09ee6af
Ponefar <adrienwolf1@gmail.com>
2023-06-07 05:59:54 +0200

    feat: #11911173 integration rules in form builder

--- packages/shared/src/types/customTables.ts
+++ packages/shared/src/types/customTables.ts
@@ -22,6 +22,7 @@ export enum DataContentType {
   OPTGROUP = 'optgroup',
   RANGEDATE = 'rangeDate',
   REQUESTTYPE = 'requestType',
+  RULEENGINE = 'ruleEngine',
 }
 
 export interface ITableHeader extends IFieldConfig {
