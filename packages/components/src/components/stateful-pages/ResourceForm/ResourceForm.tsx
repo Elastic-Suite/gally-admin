ad6a4f38adc8dc51a1c0f8736b9a775f7b2202ab
Ponefar <adrienwolf1@gmail.com>
2023-06-13 03:42:05 +0200

    feat: add error message and redirection url on resourceForm

--- packages/components/src/components/stateful-pages/ResourceForm/ResourceForm.tsx
+++ packages/components/src/components/stateful-pages/ResourceForm/ResourceForm.tsx
@@ -19,6 +19,7 @@ import { closeSnackbar, enqueueSnackbar } from 'notistack'
 import { useTranslation } from 'react-i18next'
 import styled from '@emotion/styled'
 import { Box, Theme } from '@mui/material'
+import { useRouter } from 'next/router'
 
 const CustomResourceForm = styled('div')(({ theme }) => ({
   display: 'flex',
@@ -33,10 +34,11 @@ interface IProps {
 
 function ResourceForm(props: IProps): JSX.Element {
   const { resourceName, id, categoriesList } = props
-  const { t } = useTranslation('boost')
+  const { t } = useTranslation('resourceForm')
   const resource = useResource(resourceName, IMainContext.FORM)
   const { replace, create } = useResourceOperations<any>(resource)
 
+  const router = useRouter()
   const [isLoading, setIsLoading] = useState(false)
   const requiredChamps = resource.supportedProperty.filter(
     (property) => property.required && property?.gally?.visible
@@ -74,10 +76,17 @@ function ResourceForm(props: IProps): JSX.Element {
       sendingToApi = await (create as any)(data)
     }
     if (!isError(sendingToApi)) {
-      enqueueSnackbar(t('alert'), {
+      enqueueSnackbar(id ? t('alert.update') : t('alert.create'), {
         onShut: closeSnackbar,
         variant: 'success',
       })
+      if (!id) {
+        return router.push('./grid')
+      }
+    } else {
+      throw new Error(
+        sendingToApi?.error?.message ?? sendingToApi?.violations[0]?.message
+      )
     }
     setIsLoading(false)
   }
