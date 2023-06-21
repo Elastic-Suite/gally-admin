a109b2685f371120d1ced0c991173eb2e9f85de0
Ponefar <adrienwolf1@gmail.com>
2023-06-21 17:38:13 +0200

    feat: #1236039 Add interface when no boost and refacto ResourceTable

--- packages/components/src/components/atoms/noAttributes/NoAttributes.test.tsx
+++ packages/components/src/components/atoms/noAttributes/NoAttributes.test.tsx
@@ -5,12 +5,24 @@ import { renderWithProviders } from '../../../utils/tests'
 import NoAttributes from './NoAttributes'
 
 describe('NoAttributes', () => {
-  it('match snapshot', () => {
+  it('NoAttributes with absolutLink', () => {
     const { container } = renderWithProviders(
       <NoAttributes
         title="No attributes were specified as searchable in the settings"
         btnTitle="Add searchable attributes"
         btnHref="admin/settings/attributes"
+      />
+    )
+    expect(container).toMatchSnapshot()
+  })
+
+  it('NoAttributes without absolutLink', () => {
+    const { container } = renderWithProviders(
+      <NoAttributes
+        title="No attributes were specified as searchable in the settings"
+        btnTitle="Add searchable attributes"
+        btnHref="admin/settings/attributes"
+        absolutLink={false}
       />
     )
     expect(container).toMatchSnapshot()
