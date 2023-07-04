4f4d211742552e39a2524df9085b34d5a317c195
Ponefar <adrienwolf1@gmail.com>
2023-07-04 15:10:27 +0200

    feat: #1235190 put categoriesList in requestType from jsLd

--- packages/components/src/components/stateful/RequestTypeManager/RequestTypeManager.tsx
+++ packages/components/src/components/stateful/RequestTypeManager/RequestTypeManager.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { useApiList } from '../../../hooks'
+import { useApiList, useFetchApi } from '../../../hooks'
 import RequestType from '../../atoms/form/RequestType'
 import {
+  ICategories,
   ILimitationsTypes,
   IOptionsTags,
   IRequestType,
   IRequestTypesOptions,
-  ITreeItem,
 } from '@elastic-suite/gally-admin-shared'
 
 interface IProps {
@@ -17,19 +17,12 @@ interface IProps {
   fullWidth?: boolean
   margin?: 'none' | 'dense' | 'normal'
   requestTypeConfigurations?: Record<string, string>
-  categoriesList: ITreeItem[]
   infoTooltip?: string
   placeholder?: string
 }
 
 function RequestTypeManager(props: IProps): JSX.Element | null {
-  const {
-    value,
-    onChange,
-    requestTypeConfigurations,
-    categoriesList,
-    ...args
-  } = props
+  const { value, onChange, requestTypeConfigurations, ...args } = props
 
   const [operatorOptionsApi] = useApiList<IOptionsTags>(
     requestTypeConfigurations?.operatorOptionsApi
@@ -40,10 +33,14 @@ function RequestTypeManager(props: IProps): JSX.Element | null {
   const [requestTypeOptionsApi] = useApiList<IRequestTypesOptions>(
     requestTypeConfigurations?.requestTypeOptionsApi
   )
+  const [categoriesListApi] = useFetchApi<ICategories>(
+    requestTypeConfigurations?.categoryTreeApi ?? 'categoryTree'
+  )
 
   const operatorOptions = operatorOptionsApi?.data?.['hydra:member']
   const limitationTypeOptions = limitationTypeOptionsApi?.data?.['hydra:member']
   const requestTypeOptions = requestTypeOptionsApi?.data?.['hydra:member']
+  const categoriesList = categoriesListApi?.data?.categories
 
   if (
     !(
