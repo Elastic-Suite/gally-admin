34c3a43a362aae7e4a7b1bf225441a1b5ef6e063
Matthias GOUPIL <matthiasgoup@gmail.com>
2025-02-12 11:00:25 +0100

    fix(#1330035): multiple input component doesn't initialize default value

--- packages/components/src/components/stateful/FieldGuesser/MultipleInput.tsx
+++ packages/components/src/components/stateful/FieldGuesser/MultipleInput.tsx
@@ -1,4 +1,4 @@
-import React, { SyntheticEvent, useMemo } from 'react'
+import React, { SyntheticEvent, useEffect, useMemo } from 'react'
 import {
   DataContentType,
   IFieldGuesserProps,
@@ -16,36 +16,58 @@ function MultipleInput(props: IProps): JSX.Element {
   const {
     multipleInputConfiguration: { inputDependencies },
     onChange,
+    value,
     ...otherProps
   } = props
 
-  const fields: IInputDependencies[] = useMemo(() => {
-    return (
-      Array.isArray(inputDependencies) ? inputDependencies : [inputDependencies]
-    ).filter(
-      (inputDependencie) =>
-        inputDependencie.value === otherProps.data[inputDependencie.field]
-    )
-  }, [inputDependencies, otherProps.data])
+  const fields: IInputDependencies[] = useMemo(
+    () =>
+      (Array.isArray(inputDependencies)
+        ? inputDependencies
+        : [inputDependencies]
+      ).filter(
+        (inputDependency) =>
+          inputDependency.value === otherProps.data[inputDependency.field]
+      ),
+    [inputDependencies, otherProps.data]
+  )
+
+  const defaultValue = useMemo(() => {
+    const parsedValue = value ? JSON.parse(value) : {}
+    inputDependencies.forEach((field) => {
+      if (!parsedValue[field.jsonKeyValue]) {
+        parsedValue[field.jsonKeyValue] = valueInitializer(
+          field.input,
+          field.input
+        )
+      }
+    })
+    return JSON.stringify(parsedValue)
+  }, [value, inputDependencies])
+
+  // Can be improved to avoid doing too much re-rendering by changing the behavior in the ResourceForm
+  useEffect(() => {
+    if (!value && onChange) {
+      onChange(defaultValue)
+    }
+  }, [defaultValue, value, onChange])
 
   return (
     <>
       {fields.map((field) => {
-        const value = otherProps.value
-          ? JSON.parse(otherProps.value as string)?.[field.jsonKeyValue] ??
-            valueInitializer(field.input, field.input)
-          : valueInitializer(field.input, field.input)
+        const parsedValue = JSON.parse(value || defaultValue)
+        const newValue = parsedValue[field.jsonKeyValue]
 
         function handleChange(
           _name: string,
-          value: unknown,
+          newValue: unknown,
           event?: React.SyntheticEvent<Element, Event>
         ): void {
-          const newValue: Record<string, unknown> = otherProps.value
-            ? JSON.parse(otherProps.value as string)
-            : {}
-          newValue[field?.jsonKeyValue] = value
-          onChange(JSON.stringify(newValue), event)
+          const updatedValue = {
+            ...parsedValue,
+            [field.jsonKeyValue]: newValue,
+          }
+          onChange?.(JSON.stringify(updatedValue), event)
         }
 
         return (
@@ -55,7 +77,7 @@ function MultipleInput(props: IProps): JSX.Element {
             input={field.input as DataContentType}
             editable
             id={field.input}
-            value={value}
+            value={newValue}
             onChange={handleChange}
           />
         )
