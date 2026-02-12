6f45fe26c103343ae30c30a82cbe146542e4b585
kerwan <aurelienperonnet@gmail.com>
2026-02-12 17:41:21 +0100

    fix(#1354398): add tests and ensure prefix parameter is given to page and search

--- packages/components/src/hooks/useFilters.test.ts
+++ packages/components/src/hooks/useFilters.test.ts
@@ -35,6 +35,20 @@ describe('useFilters', () => {
       )
     })
 
+    it('should redirect with parameters and prefix', () => {
+      const router = useRouter()
+      const pushSpy = router.push as jest.Mock
+      pushSpy.mockClear()
+      renderHookWithProviders(() =>
+        useFiltersRedirect(1, { foo: 'bar' }, 'baz', true, 'prefix')
+      )
+      expect(pushSpy).toHaveBeenCalledWith(
+        'http://localhost/test?prefix_foo=bar&prefix_currentPage=1&prefix_search=baz',
+        undefined,
+        { shallow: true }
+      )
+    })
+
     it('should not redirect if active is false', () => {
       const router = useRouter()
       const pushSpy = router.push as jest.Mock
@@ -60,6 +74,24 @@ describe('useFilters', () => {
       expect(result.current[0]).toEqual(42)
       router.asPath = oldPath
     })
+
+    it('should get the page state initialized from router parameter with prefix', () => {
+      const router = useRouter()
+      const oldPath = router.asPath
+      router.asPath = '/test?prefix_currentPage=42'
+      const { result } = renderHookWithProviders(() => usePage('prefix'))
+      expect(result.current[0]).toEqual(42)
+      router.asPath = oldPath
+    })
+
+    it('should get the page state initialized from router parameter only if prefix matches', () => {
+      const router = useRouter()
+      const oldPath = router.asPath
+      router.asPath = '/test?currentPage=42'
+      const { result } = renderHookWithProviders(() => usePage('prefix'))
+      expect(result.current[0]).toEqual(0)
+      router.asPath = oldPath
+    })
   })
 
   describe('useFilters', () => {
@@ -70,17 +102,65 @@ describe('useFilters', () => {
       expect(result.current[0]).toEqual({})
     })
 
-    it('should get the filters state initialized from router parameter', () => {
+    it('should get the filters state initialized from router parameter with implicit resource prefix', () => {
       const router = useRouter()
+      const prefix = `${resourceWithRef.title.toLowerCase()}_`
       const oldPath = router.asPath
-      router.asPath =
-        '/test?code=foo&metadata[]=1&metadata[]=2&isSearchable=true'
+      router.asPath = `/test?${prefix}code=foo&${prefix}metadata[]=1&${prefix}metadata[]=2&${prefix}isSearchable=true`
       const { result } = renderHookWithProviders(() =>
         useFilters(resourceWithRef)
       )
       expect(result.current[0]).toEqual({
         code: 'foo',
         'metadata[]': [1, 2],
+        isSearchable: true,
+      })
+      router.asPath = oldPath
+    })
+
+    it('should get the filters state initialized from router parameter with custom prefix', () => {
+      const router = useRouter()
+      const prefix = `customprefix_`
+      const oldPath = router.asPath
+      router.asPath = `/test?${prefix}code=foo&${prefix}metadata[]=1&${prefix}metadata[]=2&${prefix}isSearchable=true`
+      const { result } = renderHookWithProviders(() =>
+        useFilters(resourceWithRef, 'customprefix')
+      )
+      expect(result.current[0]).toEqual({
+        code: 'foo',
+        'metadata[]': [1, 2],
+        isSearchable: true,
+      })
+      router.asPath = oldPath
+    })
+
+    it('should get the filters state initialized from router parameter that matches the implicit resource prefix', () => {
+      const router = useRouter()
+      const prefix = `${resourceWithRef.title.toLowerCase()}_`
+      const oldPath = router.asPath
+      router.asPath = `/test?${prefix}code=foo&${prefix}metadata[]=1&metadata[]=2&${prefix}isSearchable=true`
+      const { result } = renderHookWithProviders(() =>
+        useFilters(resourceWithRef)
+      )
+      expect(result.current[0]).toEqual({
+        code: 'foo',
+        'metadata[]': [1],
+        isSearchable: true,
+      })
+      router.asPath = oldPath
+    })
+
+    it('should get the filters state initialized from router parameter that matches the custom resource prefix', () => {
+      const router = useRouter()
+      const prefix = `customprefix_`
+      const oldPath = router.asPath
+      router.asPath = `/test?${prefix}code=foo&metadata[]=1&${prefix}metadata[]=2&${prefix}isSearchable=true`
+      const { result } = renderHookWithProviders(() =>
+        useFilters(resourceWithRef, 'customprefix')
+      )
+      expect(result.current[0]).toEqual({
+        code: 'foo',
+        'metadata[]': [2],
         isSearchable: true,
       })
       router.asPath = oldPath
@@ -98,6 +178,15 @@ describe('useFilters', () => {
       const oldPath = router.asPath
       router.asPath = '/test?search=foo'
       const { result } = renderHookWithProviders(() => useSearch())
+      expect(result.current[0]).toEqual('foo')
+      router.asPath = oldPath
+    })
+
+    it('should get the search state initialized from router parameter with prefix', () => {
+      const router = useRouter()
+      const oldPath = router.asPath
+      router.asPath = '/test?prefix_search=foo'
+      const { result } = renderHookWithProviders(() => useSearch('prefix'))
       expect(result.current[0]).toEqual('foo')
       router.asPath = oldPath
     })
