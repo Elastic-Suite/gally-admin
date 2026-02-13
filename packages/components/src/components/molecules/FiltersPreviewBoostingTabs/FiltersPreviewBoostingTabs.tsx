d762acdec7ec1ab05afb7a1ff458bb591eb31250
kerwan <aurelienperonnet@gmail.com>
2026-02-13 16:24:18 +0100

    fix(#1354398): tabs behaviour concerning url and filters and hooks

--- packages/components/src/components/molecules/FiltersPreviewBoostingTabs/FiltersPreviewBoostingTabs.tsx
+++ packages/components/src/components/molecules/FiltersPreviewBoostingTabs/FiltersPreviewBoostingTabs.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useCallback, useMemo, useState } from 'react'
 import CustomTabs from '../layout/tabs/CustomTabs'
 import {
   IOptions,
@@ -42,110 +42,140 @@ function FiltersPreviewBoostingTabs({
 
   const [category, setCategory] = useState<ITreeItem>(null)
   const [search, setSearch] = useState('')
-  const sendFilter = (tabId: number): void => {
-    onSendFilter({
-      type: requestTypes[tabId].value,
-      search,
-      category: category != null ? category.id : null,
-    })
-  }
+  const sendFilter = useCallback(
+    (tabId: number): void => {
+      onSendFilter({
+        type: requestTypes[tabId].value,
+        search,
+        category: category != null ? category.id : null,
+      })
+    },
+    [category, onSendFilter, requestTypes, search]
+  )
 
   const [showAllErrors, setShowAllErrors] = useState(false)
 
-  const DrowDownLocalizedCatalog = (
-    <DropDown
-      onChange={(value: string): void => {
-        onLocalizedCatalogChange(value)
-        setCategory(null)
-      }}
-      value={localizedCatalog}
-      options={localizedCatalogsOptions}
-      placeholder={tCommon('localizedCatalog.placeholder')}
-      useGroups
-      required
-      showError={showAllErrors}
-      sx={{ minWidth: '252.667px' }}
-      label={tCommon('localizedCatalog')}
-    />
-  )
-
-  const handleSearch = (id: number, formIsValid: boolean): void => {
-    if (formIsValid) {
-      sendFilter(id)
-    } else {
-      setShowAllErrors(true)
-    }
-  }
-
-  const tabs: ITab<IPropsFiltersPreviewBoostingTab>[] = requestTypes.map(
-    (requestType, id) => {
-      switch (requestType.limitationType) {
-        case LimitationType.CATEGORY:
-          return {
-            label: requestType.previewLabel,
-            Component: FiltersPreviewBoostingTab,
-            id,
-            componentProps: {
-              onSearch: (formIsValid): void => handleSearch(id, formIsValid),
-              children: (
-                <>
-                  {DrowDownLocalizedCatalog}
-                  <TreeSelector
-                    showError={showAllErrors}
-                    sx={{
-                      minWidth: '252.667px',
-                    }}
-                    data={categories}
-                    onChange={(value: ITreeItem): void => {
-                      setCategory(value)
-                    }}
-                    value={category}
-                    placeholder={tBoost('selectCategory').toString()}
-                    required
-                    label={tCommon('category')}
-                  />
-                </>
-              ),
-            },
-          }
-        case LimitationType.SEARCH:
-          return {
-            label: requestType.previewLabel,
-            Component: FiltersPreviewBoostingTab,
-            id,
-            componentProps: {
-              onSearch: (formIsValid): void => handleSearch(id, formIsValid),
-              children: (
-                <>
-                  {DrowDownLocalizedCatalog}
-                  <SearchBar
-                    value={search}
-                    placeholder={tBoost('termSearch')}
-                    onChange={setSearch}
-                    showError={showAllErrors}
-                    onResearch={(formIsValid): void =>
-                      handleSearch(id, formIsValid)
-                    }
-                    label={tBoost('termSearch')}
-                  />
-                </>
-              ),
-            },
-          }
-      }
-      return null
-    }
-  )
-  return (
-    requestTypes.length > 0 && (
-      <CustomTabs
-        tabs={tabs}
-        onChange={(tabId: number): void => {
-          sendFilter(tabId)
-          setShowAllErrors(false)
+  const DrowDownLocalizedCatalog = useMemo(
+    () => (
+      <DropDown
+        onChange={(value: string): void => {
+          onLocalizedCatalogChange(value)
+          setCategory(null)
         }}
+        value={localizedCatalog}
+        options={localizedCatalogsOptions}
+        placeholder={tCommon('localizedCatalog.placeholder')}
+        useGroups
+        required
+        showError={showAllErrors}
+        sx={{ minWidth: '252.667px' }}
+        label={tCommon('localizedCatalog')}
       />
-    )
+    ),
+    [
+      localizedCatalog,
+      localizedCatalogsOptions,
+      onLocalizedCatalogChange,
+      showAllErrors,
+      tCommon,
+    ]
+  )
+
+  const handleSearch = useCallback(
+    (id: number, formIsValid: boolean): void => {
+      if (formIsValid) {
+        sendFilter(id)
+      } else {
+        setShowAllErrors(true)
+      }
+    },
+    [sendFilter]
+  )
+
+  const tabs: ITab<IPropsFiltersPreviewBoostingTab>[] = useMemo(
+    () =>
+      requestTypes.map((requestType, id) => {
+        switch (requestType.limitationType) {
+          case LimitationType.CATEGORY:
+            return {
+              label: requestType.previewLabel,
+              Component: FiltersPreviewBoostingTab,
+              id,
+              componentProps: {
+                onSearch: (formIsValid: boolean): void =>
+                  handleSearch(id, formIsValid),
+                children: (
+                  <>
+                    {DrowDownLocalizedCatalog}
+                    <TreeSelector
+                      showError={showAllErrors}
+                      sx={{
+                        minWidth: '252.667px',
+                      }}
+                      data={categories}
+                      onChange={(value: ITreeItem): void => {
+                        setCategory(value)
+                      }}
+                      value={category}
+                      placeholder={tBoost('selectCategory').toString()}
+                      required
+                      label={tCommon('category')}
+                    />
+                  </>
+                ),
+              },
+            }
+          case LimitationType.SEARCH:
+            return {
+              label: requestType.previewLabel,
+              Component: FiltersPreviewBoostingTab,
+              id,
+              componentProps: {
+                onSearch: (formIsValid: boolean): void =>
+                  handleSearch(id, formIsValid),
+                children: (
+                  <>
+                    {DrowDownLocalizedCatalog}
+                    <SearchBar
+                      value={search}
+                      placeholder={tBoost('termSearch')}
+                      onChange={setSearch}
+                      showError={showAllErrors}
+                      onResearch={(formIsValid): void =>
+                        handleSearch(id, formIsValid)
+                      }
+                      label={tBoost('termSearch')}
+                    />
+                  </>
+                ),
+              },
+            }
+        }
+        return null
+      }),
+    [
+      DrowDownLocalizedCatalog,
+      categories,
+      category,
+      handleSearch,
+      requestTypes,
+      search,
+      showAllErrors,
+      tBoost,
+      tCommon,
+    ]
+  )
+
+  const onTabChange = useCallback(
+    (tabId: number): void => {
+      sendFilter(tabId)
+      setShowAllErrors(false)
+    },
+    [sendFilter, setShowAllErrors]
+  )
+
+  return (
+    requestTypes.length > 0 && <CustomTabs tabs={tabs} onChange={onTabChange} />
   )
 }
 
