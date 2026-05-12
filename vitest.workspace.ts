e568ee909c819fbfaf133eebbf48b8ee64cff78c
kerwan <aurelienperonnet@gmail.com>
2026-05-12 10:41:36 +0200

    feat(#1386794): run tests as node and browser JS

---
+++ vitest.workspace.ts
@@ -0,0 +1,20 @@
+import { defineWorkspace } from 'vitest/config'
+
+export default defineWorkspace([
+  {
+    extends: 'vitest.config.ts',
+    test: {
+      name: 'node',
+      environment: 'node',
+      include: ['tests/**/*.test.ts'],
+    },
+  },
+  {
+    extends: 'vitest.config.ts',
+    test: {
+      name: 'browser',
+      environment: 'happy-dom',
+      include: ['tests/**/*.test.ts'],
+    },
+  },
+])
